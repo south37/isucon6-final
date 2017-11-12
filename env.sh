e040a438dfea0db8ddb910b926208488b78ad892
Nao Minami <south37777@gmail.com>
2017-11-12 08:52:07 +0000

    Update react

--- env.sh
+++ env.sh
@@ -6,6 +6,7 @@ MYSQL_USER=isucon
 MYSQL_PASS=isucon
 
 # For react
+NODE_ENV=production
 SSL_KEY=/home/isucon/webapp/ssl/oreore.key 
 SSL_CERT=/home/isucon/webapp/ssl/oreore.crt 
 API=http://localhost:8080
