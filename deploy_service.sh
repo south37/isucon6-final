a0876d0a64a918a4a8e901f16ab68b0c4b298c32
Nao Minami <south37777@gmail.com>
2017-11-12 08:49:43 +0000

    Add isu.react.service

--- deploy_service.sh
+++ deploy_service.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo cp "$HOME/isu.ruby.service" "/etc/systemd/system/isu.ruby.service"
+sudo cp "$HOME/isu.react.service" "/etc/systemd/system/isu.react.service"
 sudo systemctl daemon-reload
--- env.sh
+++ env.sh
@@ -1,1 +1,11 @@
+# For ruby
+ISUCON_ENV=production
 MYSQL_HOST=54.199.234.200
+MYSQL_PORT=3306
+MYSQL_USER=isucon
+MYSQL_PASS=isucon
+
+# For react
+SSL_KEY=/home/isucon/webapp/ssl/oreore.key 
+SSL_CERT=/home/isucon/webapp/ssl/oreore.crt 
+API=http://localhost:8080
