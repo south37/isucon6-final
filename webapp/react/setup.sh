c7714ec9673dc0c3a85f2889761973d8bc867c47
Nao Minami <south37777@gmail.com>
2017-11-12 07:59:08 +0000

    Update setup.sh

--- webapp/react/setup.sh
+++ webapp/react/setup.sh
@@ -1,9 +1,9 @@
-etup react
-sudo apt install -y nodejs nodejs-legacy npm
-cd "$HOME/webapp/react"
-npm install && npm cache clean
-NODE_ENV=production npm run build
+# setup react
+# sudo apt install -y nodejs nodejs-legacy npm
+# cd "$HOME/webapp/react"
+# npm install && npm cache clean
 
 # Start react
 cd "$HOME/webapp/react"
-node build/server.js
+NODE_ENV=production npm run build
+SSL_KEY=/home/isucon/webapp/ssl/oreore.key SSL_CERT=/home/isucon/webapp/ssl/oreore.crt API=http://localhost:8080 node build/server.js
--- webapp/ruby/setup.sh
+++ webapp/ruby/setup.sh
@@ -8,6 +8,6 @@ bundle install --jobs 4
 # Setup timezone of mysql by http://ikm.hatenablog.jp/entry/2013/06/26/185518
 
 # Start ruby
-# MYSQL_HOST is web01
+# MYSQL_HOST is web03. 54.199.234.200
 cd "$HOME/webapp/ruby"
-MYSQL_HOST=54.238.211.35 bundle exec thin start --port 8080 -A rack -R config.ru -e production
+MYSQL_HOST=54.199.234.200 bundle exec thin start --port 8080 -A rack -R config.ru -e production
