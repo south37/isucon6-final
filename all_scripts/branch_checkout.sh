fb674775ba149aa3363071b47cb453d022989f34
Nao Minami <south37777@gmail.com>
2017-11-19 14:25:05 +0900

    Add branch_checkout

--- all_scripts/branch_checkout.sh
+++ all_scripts/branch_checkout.sh
@@ -6,9 +6,14 @@ HOST3="54.250.245.52"
 HOST4="54.199.141.147"
 HOST5="54.238.182.113"
 
+if [ "$#" -ne 1 ]; then
+  echo "You must specify branch!"
+  exit 1
+fi
+
 HOSTS="${HOST1} ${HOST2} ${HOST3} ${HOST4} ${HOST5}"
-COMMAND="git status"
-echo "Check branch status..."
+COMMAND="git checkout $1"
+echo "Checkout branch..."
 echo "COMMAND: ${COMMAND}"
 i=1
 for host in ${HOSTS[@]}; do echo "" && echo "HOST${i}: ${host}"; ssh "isucon@${host}" "${COMMAND}"; i=$((i+1)); done
