e12a307e5653b761ae0dbf6c8797fa341063d2a5
Kent Nagata <ngtknt@me.com>
2017-11-19 18:52:27 +0900

    Fix

--- test/main.rb
+++ test/main.rb
@@ -3,6 +3,7 @@ require "faraday_middleware"
 require "active_support/notifications"
 require "logger"
 require "pry"
+require "json"
 
 url = 'http://localhost:9292'
 c = Faraday.new(url: url) do |faraday|
@@ -17,11 +18,28 @@ ActiveSupport::Notifications.subscribe('request.faraday') do |name, starts, ends
   http_method = env[:method].to_s.upcase
   time_sec = ends - starts
   time_ms = time_sec * 1000
-  logger.info "#{http_method} #{url} in #{time_ms}ms"
+  http_status = env[:response].status
+  if http_status > 200
+    logger.error "#{http_status} #{http_method} #{url} in #{time_ms}ms"
+  else
+    logger.info "#{http_status} #{http_method} #{url} in #{time_ms}ms"
+  end
 end
 
 
 # ------ Test -------
 c.get "/initialize"
-c.get "/rooms"
-c.get "/rooms/1"
+
+# Get token
+res = c.post "/api/csrf_token"
+token = JSON.parse(res.body)["token"]
+
+# post room
+res = c.post "/api/rooms" do |req|
+  req.headers['X_CSRF_TOKEN'] = token
+  req.body = {
+    name: "test",
+    canvas_width: 1000,
+    canvas_height: 800,
+  }.to_json
+end
