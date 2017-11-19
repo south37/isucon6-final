bce01cc8c6be78908c1fd763fe308511057359e5
Nao Minami <south37777@gmail.com>
2017-11-19 15:19:06 +0900

    Update service

--- all_scripts/deploy.sh
+++ all_scripts/deploy.sh
@@ -17,13 +17,19 @@ NGINX_COMMAND="hostname && git fetch origin ${TARGET} && git checkout ${TARGET} 
 NGINX_HOSTS="${HOST2}"
 echo "Deploy nginx..."
 echo "COMMAND: ${NGINX_COMMAND}"
-for i in ${NGINX_HOSTS[@]}; do echo "" && echo $i; ssh "isucon@${i}" "${NGINX_COMMAND}"; done
+for i in ${NGINX_HOSTS[@]}; do
+  echo ""
+  echo $i; ssh "isucon@${i}" "${NGINX_COMMAND}"
+done
 echo "Deployed nginx!"
 echo ""
 
-WEB_COMMAND="hostname && git fetch origin ${TARGET} && git checkout ${TARGET} && git pull origin ${TARGET} && /home/isucon/scripts/deploy_app.sh"
+WEB_COMMAND="hostname && git fetch origin ${TARGET} && git checkout ${TARGET} && git pull origin ${TARGET} && /home/isucon/scripts/deploy_service.sh /home/isucon/scripts/deploy_app.sh"
 WEB_HOSTS="${HOST1} ${HOST2} ${HOST4} ${HOST5}"
 echo "Deploy app..."
 echo "COMMAND: ${WEB_COMMAND}"
-for i in ${WEB_HOSTS[@]}; do echo "" && echo $i; ssh "isucon@${i}" "${WEB_COMMAND}"; done
+for i in ${WEB_HOSTS[@]}; do
+  echo ""
+  echo $i; ssh "isucon@${i}" "${WEB_COMMAND}"
+done
 echo "Deployed app!"
