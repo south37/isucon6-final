493374b97eeac2bb17f47b28eac1851b80fa2dde
Kent Nagata <ngtknt@me.com>
2017-11-19 16:52:25 +0900

    Add test

---
+++ test/main.rb
@@ -0,0 +1,25 @@
+require "faraday"
+require "faraday_middleware"
+require "active_support/notifications"
+require "logger"
+require "pry"
+
+url = 'http://localhost:9292'
+c = Faraday.new(url: url) do |faraday|
+  faraday.request :url_encoded
+  faraday.use FaradayMiddleware::Instrumentation
+  faraday.adapter Faraday.default_adapter
+end
+
+logger = Logger.new(STDOUT)
+ActiveSupport::Notifications.subscribe('request.faraday') do |name, starts, ends, _, env|
+  url = env[:url]
+  http_method = env[:method].to_s.upcase
+  time_sec = ends - starts
+  time_ms = time_sec * 1000
+  logger.info "#{http_method} #{url} in #{time_ms}ms"
+end
+
+
+# ------ Test -------
+c.get "/initialize"
