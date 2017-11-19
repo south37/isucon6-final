0cebbd4e155e5d7f1e5fa6e5ea7728e835168c45
Nao Minami <south37777@gmail.com>
2017-11-19 14:26:24 +0900

    Update deploy.sh

--- all_scripts/deploy.sh
+++ all_scripts/deploy.sh
@@ -13,7 +13,7 @@ if [ "$#" = 1 ]; then
   TARGET="$1"
 fi
 
-NGINX_COMMAND="hostname && git pull origin ${TARGET} && /home/isucon/scripts/deploy_nginx.sh"
+NGINX_COMMAND="hostname && git fetch origin ${TARGET} && git checkout ${TARGET} && git pull origin ${TARGET} && /home/isucon/scripts/deploy_nginx.sh"
 NGINX_HOSTS="${HOST2}"
 echo "Deploy nginx..."
 echo "COMMAND: ${NGINX_COMMAND}"
@@ -21,7 +21,7 @@ for i in ${NGINX_HOSTS[@]}; do echo "" && echo $i; ssh "isucon@${i}" "${NGINX_CO
 echo "Deployed nginx!"
 echo ""
 
-WEB_COMMAND="hostname && git pull origin ${TARGET} && /home/isucon/scripts/deploy_app.sh"
+WEB_COMMAND="hostname && git fetch origin ${TARGET} && git checkout ${TARGET} && git pull origin ${TARGET} && /home/isucon/scripts/deploy_app.sh"
 WEB_HOSTS="${HOST1} ${HOST2} ${HOST4} ${HOST5}"
 echo "Deploy app..."
 echo "COMMAND: ${WEB_COMMAND}"
