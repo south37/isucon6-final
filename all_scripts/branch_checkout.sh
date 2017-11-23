9d4505987624cff4d6f3ba02b163528b8fcf9981
Nao Minami <south37777@gmail.com>
2017-11-23 11:20:19 +0900

    Update all_scripts

--- all_scripts/branch_checkout.sh
+++ all_scripts/branch_checkout.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-HOST1="54.238.174.46"
-HOST2="13.114.230.223"
-HOST3="54.250.245.52"
-HOST4="54.199.141.147"
-HOST5="54.238.182.113"
+. "$(pwd)/all_scripts/hosts.txt"
 
 if [ "$#" -ne 1 ]; then
   echo "You must specify branch!"
   exit 1
 fi
 
-HOSTS="${HOST1} ${HOST2} ${HOST3} ${HOST4} ${HOST5}"
 COMMAND="git checkout $1"
 echo "Checkout branch..."
 echo "COMMAND: ${COMMAND}"
--- all_scripts/deploy.sh
+++ all_scripts/deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-HOST1="54.238.174.46"
-HOST2="13.114.230.223"
-HOST3="54.250.245.52"
-HOST4="54.199.141.147"
-HOST5="54.238.182.113"
+. "$(pwd)/all_scripts/hosts.txt"
 
 TARGET="master"
 
@@ -16,7 +12,6 @@ fi
 LOAD_COMMAND="git checkout master && git pull origin master && git fetch origin ${TARGET} && git checkout ${TARGET} && git pull origin ${TARGET} && git merge master"
 
 NGINX_COMMAND="hostname && ${LOAD_COMMAND} && /home/isucon/scripts/deploy_nginx.sh"
-NGINX_HOSTS="${HOST2}"
 echo "Deploy nginx..."
 echo "COMMAND: ${NGINX_COMMAND}"
 for i in ${NGINX_HOSTS[@]}; do
@@ -28,7 +23,6 @@ echo "Deployed nginx!"
 echo ""
 
 WEB_COMMAND="hostname && ${LOAD_COMMAND} && /home/isucon/scripts/deploy_service.sh && /home/isucon/scripts/deploy_app.sh"
-WEB_HOSTS="${HOST1} ${HOST2} ${HOST4} ${HOST5}"
 echo "Deploy app..."
 echo "COMMAND: ${WEB_COMMAND}"
 for i in ${WEB_HOSTS[@]}; do
