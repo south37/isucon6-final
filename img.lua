7d79d229762ef7efe3e70681b695bd77f347bef3
Rei Kubonaga <kubonagarei@gmail.com>
2017-11-23 12:07:56 +0900

    Add img.lua

---
+++ img.lua
@@ -0,0 +1,25 @@
+local redis = require "resty.redis"
+local red = redis:new()
+
+red:set_timeout(1000)
+
+local uri = ngx.var.request_uri
+local ok, err = red:connect("127.0.0.1", 6379)
+if not ok then
+   ngx.say("failed to connect: ", err)
+   return
+end
+local res, err = red:get(uri)
+if not res then
+   ngx.say("failed to get: ", error)
+   return
+end
+if res == ngx.null then
+  local res = ngx.location.capture("/proxy"..uri)
+  ngx.header.content_type = res.content_type
+  ngx.status = res.status
+  ngx.say(res.body)
+  return
+end
+ngx.say(res)
+return
