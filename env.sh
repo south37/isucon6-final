d1f501475b8603b4460eab747508b66483658382
Nao Minami <south37777@gmail.com>
2017-11-23 13:10:44 +0900

    Update MYSQL_HOST and REDIS_HOST

--- env.sh
+++ env.sh
@@ -1,14 +1,14 @@
 # For ruby
 ISUCON_ENV=production
-MYSQL_HOST=54.250.245.52
+MYSQL_HOST=13.113.191.11
 MYSQL_PORT=3306
 MYSQL_USER=isucon
 MYSQL_PASS=isucon
-REDIS_HOST=54.250.245.52
+REDIS_HOST=13.113.191.11
 
 # For react
 NODE_ENV=production
-SSL_KEY=/home/isucon/webapp/ssl/oreore.key 
-SSL_CERT=/home/isucon/webapp/ssl/oreore.crt 
+SSL_KEY=/home/isucon/webapp/ssl/oreore.key
+SSL_CERT=/home/isucon/webapp/ssl/oreore.crt
 API=http://localhost:8080
 REACT_PORT=3000
