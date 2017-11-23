fcac6f27502ff566a749949d8f8447aa10e8b4db
Nao Minami <south37777@gmail.com>
2017-11-23 17:19:33 +0900

    Add bundle option

--- scripts/deploy_app.sh
+++ scripts/deploy_app.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+for OPT in "$@"; do
+  case $OPT in
+    '--bundle' )
+        FLAG_BUNDLE=1
+        ;;
+  esac
+  shift
+done
+
+if [ $FLAG_BUNDLE ]; then
+  BUNDLE_OPTION="--bundle"
+else
+  BUNDLE_OPTION=""
+fi
+
 echo 'Restart services...'
-  "$HOME/scripts/deploy_ruby.sh"
+  "$HOME/scripts/deploy_ruby.sh ${BUNDLE_OPTION}"
   "$HOME/scripts/deploy_react.sh"
 echo 'Restarted!'
