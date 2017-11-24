e978039f9ba8fc331872273392ea9871d4632524
Nao Minami <south37777@gmail.com>
2017-11-24 10:25:47 +0900

    Update log.sh

--- all_scripts/log.sh
+++ all_scripts/log.sh
@@ -2,7 +2,11 @@
 
 . "$(pwd)/all_scripts/hosts.txt"
 
-COMMAND="hostname && /home/isucon/scripts/log_ruby.sh"
+TAIL_LENGTH=10
+
+# TODO(south37) Add tail options
+
+COMMAND="hostname && ( /home/isucon/scripts/log_ruby.sh | tail -n ${TAIL_LENGTH} ) && ( /home/isucon/scripts/log_react.sh | tail -n ${TAIL_LENGTH} )"
 for i in ${HOSTS[@]}; do
   echo ""
   echo $i
--- scripts/log_react.sh
+++ scripts/log_react.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo journalctl -u isu.react.service
+echo "isucon" | sudo -S journalctl -u isu.react.service
